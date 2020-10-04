f5c6074cfca4a8f273549f4ab3e6bc24fdfd3dfe
Max Bogovic <maxim.bogovik@almexoft.com>
2020-10-04 08:15:09 +0300

    init

---
+++ src/main/java/com/ithillel/service/InMemoryTextProcessor.java
@@ -0,0 +1,27 @@
+package com.ithillel.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class InMemoryTextProcessor implements TextProcessor {
+
+    private Map<String, String> storage = new HashMap() ;
+
+    public void save(String key, final String text) {
+        if (isEmpty(key)) throw new IllegalArgumentException("key must be a set");
+        if (isEmpty(text)) return;
+        storage.compute(key, (k, value) -> (value == null ? text : "\n" + text));
+    }
+
+    private boolean isEmpty(String key) {
+        return Objects.isNull(key) || key.isEmpty();
+    }
+
+    public String getByKey(String key) {
+        if (isEmpty(key)) throw new IllegalArgumentException("key must be a set");
+        String text = storage.get(key);
+        if (text == null ) throw new IllegalStateException("key not found");
+        return text;
+    }
+}
